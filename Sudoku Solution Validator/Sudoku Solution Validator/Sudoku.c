4273446c3da1655a3d30502115245fddc53971f6
HongYD <nirvanagun93@gmail.com>
2017-10-28 23:08:12 +0900

    Add some functions

--- Sudoku Solution Validator/Sudoku Solution Validator/Sudoku.c
+++ Sudoku Solution Validator/Sudoku Solution Validator/Sudoku.c
@@ -1,9 +1,17 @@
 #include "Sudoku.h"
 
+int UNSOLVED = 81;
+int SIZE_ROWS = 9;
+int SIZE_COLUMNS = 9;
+
+
 int main()
 {
 	int** puzzle;
+	Square*** sudoku;
 	puzzle = createPuzzle();
 	printPuzzle(puzzle);
+	sudoku = setUpPuzzle(puzzle);
+	checkPuzzle(sudoku);
 	return 0;
 }
